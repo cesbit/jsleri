709f8115216ab9e4c48cad4a04f882834f5c54f0
Jeroen van der Heijden <jvdheijden@insign.it>
2016-05-20 10:56:08 +0200

    added json test

--- test.js
+++ test.js
@@ -4,40 +4,71 @@
  * This grammar is generated using the Grammar.export_js() method and
  * should be used with the jsleri JavaScript module.
  *
- * Source class: _TestGrammar4
- * Created at: 2016-05-20 08:29:34
+ * Source class: JsonGrammar
+ * Created at: 2016-05-20 10:51:17
  */
 
 'use strict';
 
 (function (
+            List,
+            Token,
+            Regex,
+            Keyword,
             Sequence,
             Choice,
-            Token,
-            List,
             Ref,
-            Grammar,
-            Keyword
+            Grammar
         ) {
-    var START = Ref(Sequence),
-        ni_item = Choice(Keyword('ni'), START),
-        ni_seq = Sequence('[', List(ni_item), ']');
-    Object.assign(START, ni_seq);
+    var START = Ref(Choice);
+    var r_string = Regex('^(")(?:(?=(\\\\?))\\2.)*?\\1');
+    var r_float = Regex('^-?[0-9]+\\.?[0-9]+');
+    var r_integer = Regex('^-?[0-9]+');
+    var k_true = Keyword('true');
+    var k_false = Keyword('false');
+    var k_null = Keyword('null');
+    var json_map_item = Sequence(
+        r_string,
+        Token(':'),
+        START
+    );
+    var json_map = Sequence(
+        Token('{'),
+        List(json_map_item, Token(','), 0, undefined, false),
+        Token('}')
+    );
+    var json_array = Sequence(
+        Token('['),
+        List(START, Token(','), 0, undefined, false),
+        Token(']')
+    );
+    Object.assign(START, Choice(
+        r_string,
+        r_float,
+        r_integer,
+        k_true,
+        k_false,
+        k_null,
+        json_map,
+        json_array
+    ));
 
-    window._TestGrammar4 = Grammar(START);
+    window.JsonGrammar = Grammar(START, '^\\w+');
 
 })(
+    window.jsleri.List,
+    window.jsleri.Token,
+    window.jsleri.Regex,
+    window.jsleri.Keyword,
     window.jsleri.Sequence,
     window.jsleri.Choice,
-    window.jsleri.Token,
-    window.jsleri.List,
     window.jsleri.Ref,
-    window.jsleri.Grammar,
-    window.jsleri.Keyword
+    window.jsleri.Grammar
 );
 
 
-(function (Grammar) {
-    window.console.log(Grammar.parse('[ni, ni, [ni, [], [ni, ni]]'));
 
-})(window._TestGrammar4);
+(function (Grammar) {
+    window.console.log(Grammar.parse('{"hoi \\"Iris\\"": .5}'));
+
+})(window.JsonGrammar);
