d1ba6fe29663701de184c54c4910c4bc2a1382f5
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-01-22 09:06:30 +0100

    Solved vulnerability, replace uglifyjs with terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 /* global require, __dirname, module, process */
 const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
 const VERSION = require(path.resolve(__dirname, './package.json')).version;
 
@@ -33,13 +33,12 @@ const config = {
     ],
     optimization: {
         minimizer: [
-            new UglifyJSPlugin({
-                uglifyOptions: {
-                    compress: {
-                        warnings: true
-                    }
-                }
-            })
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 5,
+                },
+            }),
         ]
     }
 };
